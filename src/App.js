a99c356a8f149b2547bb633538c6e316c5de8b7a
Milosz214234t4 <78423365+Milosz214234t4@users.noreply.github.com>
2022-07-27 23:01:57 +0200

    porawnie miesza i ustawia key po wymieszaniu

--- src/App.js
+++ src/App.js
@@ -1,50 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useState, useEffect} from 'react';
+import Game from './components/Game';
 function App() {
-  const [counter,setCounter] = useState(0)
+
+  //w wymieszanej tablicy wymienic kazdy i dac mu name takie jak ma jak bedzie x (petla for dla arr i setatribute),
+  //jak name i id bedzie sie w kazdym zgadzac to wygrales
+  const [counter,setCounter] = useState(0);
+  const [game, setGame] = useState([])
   let arr = []
 
   const handleClick = event => {
-    console.log(event.currentTarget.id);
+    // console.log(event.currentTarget.id);
     if(arr.length < 1){
       arr.push(event.currentTarget.id)
-      console.log(arr)
+      // console.log(arr)
     }
     else{
       arr.push(event.currentTarget.id);
-      console.log(arr);
+      // console.log(arr);
       changeid(arr);
       arr = [];
     }
   };
+  let arrelements = [];
+for(let x = 0; x <5; x++){
+  arrelements.push(React.createElement('div', {className: "div", id: x,  onClick: (e) => {
+    // cleanarray(indexarr);
+   handleClick(e)
+   
+  }, }));
+}
  function changeid(arr)
  {
 
   let buttons = document.querySelectorAll(".div");
-  console.log(arr);
+  // console.log(arr);
   console.log("jestem w funkcji");
+  
   for(Element of arr){
-   console.log(Element.id)
+  //  console.log(Element.id)
   }
   for(Element of buttons){
     if(Element.id == arr[1] || Element.id == arr[0]){
       if(Element.id== arr[1]) {
-        console.log(arr[1].id);
+        // console.log(arr[1].id);
         
       // console.log(Element);
       console.log("warunek 1");
-      console.log(arr[1])
-      console.log(Element.id)
+      // console.log(arr[1])
+      // console.log(Element.id)
       Element.setAttribute('id',arr[0]);
       console.log(Element)
       }  
        else if (Element.id == arr[0]){
         // console.log(Element);
-        console.log(arr[0])
+        // console.log(arr[0])
         console.log("warunek drugi")
       Element.setAttribute('id', arr[1]);
-      console.log(Element)
+      // console.log(Element)
       }
     }
 
@@ -55,7 +69,7 @@ function App() {
 
   return (
     <>
-    <div
+    {/* <div
     className='div'
     id = "1"
     key= "1"
@@ -93,8 +107,14 @@ name="5"
 onClick={(e) => {          handleClick(e)
 
 }}  
-       />     
+       />      */}
+
 <h1>{counter}</h1>
+{/* {arrelements} */}
+<hr/>
+<Game board={arrelements}
+  game={game}
+  setGame={setGame}/>
     </>
   );
 }
---
+++ src/components/Game.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+function Game({ board, game, setGame }) {
+  // console.log(arrcheck);
+  var list;
+ 
+  useEffect(() => {
+    list = board.sort(() => Math.random() - 0.5);
+    console.log(list);
+   
+    setGame(board.sort(() => Math.random() - 0.5));
+    // console.log(game)
+  }, []);
+  useEffect(() => {
+    let a = document.querySelectorAll(".div");
+
+    for(let x  = 0; x <a.length; x++){
+      console.log(a[x])
+      // console.log(a[x]._owner.return);
+      a[x].setAttribute('key',x);
+    }
+  }, [game]);
+  
+  
+  return <>{game}</>;
+}
+
+export default Game;
