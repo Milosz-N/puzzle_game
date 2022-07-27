acc3c7deef3dcf7c17f9f1957cdfd873a9b63533
Milosz214234t4 <78423365+Milosz214234t4@users.noreply.github.com>
2022-07-27 21:31:04 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, {useState, useEffect} from 'react';
 function App() {
+  const [counter,setCounter] = useState(0)
+  let arr = []
+
+  const handleClick = event => {
+    console.log(event.currentTarget.id);
+    if(arr.length == 0){
+      arr.push(event.currentTarget)
+      console.log(arr)
+    }
+    else{
+      arr.push(event.currentTarget);
+      console.log(arr);
+      changeid(arr);
+      arr = [];
+    }
+  };
+ function changeid(arr)
+ {
+  let buttons = document.querySelectorAll(".div");
+  console.log(arr);
+  console.log("jestem w funkcji");
+  for(Element of arr){
+   console.log(Element.id)
+  }
+  for(Element of buttons){
+    if(Number.parseInt(Element.id) == Number.parseInt(arr[0].id) ||Number.parseInt(Element.id) == Number.parseInt(arr[1].id) ){
+      console.log(Element);
+    }
+   }
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div
+    className='div'
+    id = "1"
+    key= "1"
+    name="1"
+    onClick={(e) => {         handleClick(e)
+
+          }}  
+        />
+    <div id = "2"
+        className='div'
+name="2"
+     key= "2"
+     onClick={(e) => {          handleClick(e)
+
+     }}  
+        />
+    <div id = "3" key= "3"
+        className='div'
+name="3"
+onClick={(e) => {          handleClick(e)
+
+}}  
+  />    <div id = "4"
+     key= "4"
+     className='div'
+name="4"
+onClick={(e) => {          handleClick(e)
+
+}}  
+       />     
+        <div id = "5"
+     key= "5"
+     className='div'
+name="5"
+onClick={(e) => {          handleClick(e)
+
+}}  
+       />     
+<h1>{counter}</h1>
+    </>
   );
 }
 
